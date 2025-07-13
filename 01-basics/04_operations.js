c35ab85b3b510ee54b398b23c7e7461e3e6fec55
ABHI <physicsevil007@gmail.com>
2025-07-14 04:33:43 +0530

    operations

---
+++ 01-basics/04_operations.js
@@ -0,0 +1,31 @@
+//********* operations ******************//
+/*let val=3
+let negval= -val
+console.log(negval);*/
+
+
+/*console.log(2**8)
+console.log(2%3)
+console.log(2/3);
+console.log(3*8);*/
+
+/*let str1="abhijit"
+let str2=" dutta"
+let str3=str1+str2
+console.log(str3);*/
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log();
+
+
+
+
+
+
+
+
+
