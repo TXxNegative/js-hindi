9dce164712d44b02e36c7816d975e7870c2cb1ea
ABHI <physicsevil007@gmail.com>
2025-07-14 06:50:29 +0530

    comparision

---
+++ 01-basics/05_comparision.js
@@ -0,0 +1,14 @@
+/*console.log(null > 0);
+console.log(null == 0); //in this operator null act as a null
+console.log(null >= 0); //in this null act as a 0(for all comparison operators)
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined >= 0); //false*/
+
+// STRICT CHECK (===)(used for compare two different data type) //
+console.log("2"===2);
+
+
+
+
