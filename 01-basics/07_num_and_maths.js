a55cfdd6190bba8fbfebef28303d91a5f8a1c3c7
ABHI <physicsevil007@gmail.com>
2025-07-15 06:57:53 +0530

    random maths(module)

---
+++ 01-basics/07_num_and_maths.js
@@ -0,0 +1,35 @@
+// NUMBERS //
+/*const score=400
+console.log(score);
+
+
+const balance = new Number(100);
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const othnum=23.4445353
+console.log(othnum.toPrecision(4));
+
+const hun=1000000
+console.log(hun.toLocaleString())
+
+console.log(Number.MAX_SAFE_INTEGER);*/
+
+// MATHS(library) //
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(6.7));
+console.log(Math.ceil(6.2));//give the int at the right side of the number line
+console.log(Math.floor(5.9))//give the int at the left side of the number line
+console.log(Math.sin(0));
+console.log(Math.max(90,5,6,79));
+console.log(Math.min(90,5,6,79));
+console.log(Math.random())
+console.log((Math.random()*10) +1)// +1 for avoiding case of 0
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max=20
+
