117a3a17759f3d1ac78a8370c654f43dd4ac910d
ABHI <physicsevil007@gmail.com>
2025-07-17 05:25:40 +0530

    Update 07_num_and_maths.js
    nice

--- 01-basics/07_num_and_maths.js
+++ 01-basics/07_num_and_maths.js
@@ -30,6 +30,7 @@ console.log(Math.random())
 console.log((Math.random()*10) +1)// +1 for avoiding case of 0
 console.log(Math.floor(Math.random()*10)+1)
 
-const min=10
+const min=13
 const max=20
+console.log(Math.floor(Math.random() * (max - min + 1) + min)) // random number between min and max
 
