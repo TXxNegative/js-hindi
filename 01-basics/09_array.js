c809c2894268bce1c8587da06f3c2e42833e0ef2
ABHI <physicsevil007@gmail.com>
2025-07-18 12:10:21 +0530

    Update 09_array.js
    array

--- 01-basics/09_array.js
+++ 01-basics/09_array.js
@@ -1,3 +1,14 @@
 //arrays
 
-const myarray = [0,1,2,3,4,5]
+const myarray = [0,1,2,3,4,5,true,"hitesh"]//array can have differrent data type element
+const myhero=["hulk","iron man"]
+const mop=new Array(9,0,8,78)
+
+console.log(myarray[7])
+
+//array methods
+myarray.push(7)//enter element at the end of the list
+console.log(myarray);
+myarray.pop()//remove one element from the end of the list
+console.log(myarray);
+
