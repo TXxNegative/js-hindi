da8fce77f61ebeeb46d3c1eaa3f3fb6b9db5e059
ABHI <physicsevil007@gmail.com>
2025-07-19 08:45:31 +0530

    array1

--- 01-basics/09_array.js
+++ 01-basics/09_array.js
@@ -1,7 +1,7 @@
 //arrays
 
 const myarray = [0,1,2,3,4,5,true,"hitesh"]//array can have differrent data type element
-const myhero=["hulk","iron man"]
+/*const myhero=["hulk","iron man"]
 const mop=new Array(9,0,8,78)
 
 console.log(myarray[7])
@@ -11,4 +11,26 @@ myarray.push(7)//enter element at the end of the list
 console.log(myarray);
 myarray.pop()//remove one element from the end of the list
 console.log(myarray);
+myarray.unshift(89)//it is not optimised ,because first it shift the all elements toward right then it enter the element at first
+console.log(myarray);
+myarray.shift()
+console.log(myarray);
+myarray.shift()
+console.log(myarray);
+console.log(myarray.includes(9));//datatype boolean*/
+
+/*const newarr=myarray.join()//conver the array in string form 
+console.log(newarr)
+console.log(myarray);*/
+
+//slice,splice difference
+/*console.log("A",myarray);
+const l0=myarray.slice(1,3)//show element at possition 1,2 not 3 of the array
+console.log(l0);
+console.log("B",myarray)
+
+console.log("A",myarray);
+const l1=myarray.splice(1,3)//show element at possition 1,2,3 AND ALSO REMOVE THE ELEMENTS FROM THE ARRAY 
+console.log(l1);
+console.log("B",myarray)*/
 
