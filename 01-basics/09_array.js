7d36fe27405d524930caadceacf935fa99e6b68c
ABHI <physicsevil007@gmail.com>
2025-07-20 10:10:20 +0530

    Update 09_array.js
    array 3

--- 01-basics/09_array.js
+++ 01-basics/09_array.js
@@ -1,7 +1,7 @@
 //arrays
 
-const myarray = [0,1,2,3,4,5,true,"hitesh"]//array can have differrent data type element
-/*const myhero=["hulk","iron man"]
+/*const myarray = [0,1,2,3,4,5,true,"hitesh"]//array can have differrent data type element
+const myhero=["hulk","iron man"]
 const mop=new Array(9,0,8,78)
 
 console.log(myarray[7])
@@ -13,7 +13,7 @@ myarray.pop()//remove one element from the end of the list
 console.log(myarray);
 myarray.unshift(89)//it is not optimised ,because first it shift the all elements toward right then it enter the element at first
 console.log(myarray);
-myarray.shift()
+myarray.shift()//remove first element of the array
 console.log(myarray);
 myarray.shift()
 console.log(myarray);
@@ -33,4 +33,27 @@ console.log("A",myarray);
 const l1=myarray.splice(1,3)//show element at possition 1,2,3 AND ALSO REMOVE THE ELEMENTS FROM THE ARRAY 
 console.log(l1);
 console.log("B",myarray)*/
+
+const marvel=["hulk","ironman","thor"]
+const dc=["flash","superman","batman"]
+
+/*marvel.push(dc);//in this way we are pusshing the whole array inside another array, not individual element([ 'hulk', 'ironman', 'thor', [ 'flash', 'superman', 'batman' ] ])
+console.log(marvel);*/
+
+/*const allhero=marvel.concat(dc)// in this method we are pusshin the each element in the array
+console.log(allhero);*/
+
+const allnewheros=[...marvel,...dc]//concatanate two arrays using spread
+console.log(allnewheros);
+
+const anotherarr=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const useantarr=anotherarr.flat(Infinity)//it will remove all array inside the array and create a single array using those values
+console.log(anotherarr);
+console.log(useantarr);
+
+console.log(Array.isArray("dick"));
+const dj=Array.from("nigga")
+console.log(Array.from("dick"));
+console.log(Array.from({name:"dick"}))
+
 
