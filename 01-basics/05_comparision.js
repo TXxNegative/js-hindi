14cb7acb16c13414e33fc8fc7d4d5e947707f586
ABHI <physicsevil007@gmail.com>
2025-12-15 15:26:35 +0530

    revision comparison

--- 01-basics/05_comparision.js
+++ 01-basics/05_comparision.js
@@ -1,8 +1,10 @@
-/*console.log(null > 0);
+console.log(null > 0);//false
 console.log(null == 0); //in this operator null act as a null
 console.log(null >= 0); //in this null act as a 0(for all comparison operators)
+console.log(null <= 0);//true
+console.log(null < 0);//false
 
-console.log(undefined == 0); //false
+/*console.log(undefined == 0); //false
 console.log(undefined > 0); //false
 console.log(undefined >= 0); //false*/
 
