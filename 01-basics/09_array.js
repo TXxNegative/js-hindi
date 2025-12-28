988546eac19ff4127c87d712848a805eaa2365da
ABHI <physicsevil007@gmail.com>
2025-12-29 03:59:10 +0530

    last

--- 01-basics/09_array.js
+++ 01-basics/09_array.js
@@ -59,5 +59,5 @@ console.log(Array.from({name:"dick"}))// it will show an empty array
 let s1=100
 let s2=200
 let s3=300
-console.log(Array.of(s1,s2,s3));
+console.log(Array.of(s1,s2,s3));//like concatanation
 
