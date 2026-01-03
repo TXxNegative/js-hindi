3482e0d0243c685e2733acc9f3276a4f504013a1
ABHI <physicsevil007@gmail.com>
2026-01-04 05:11:31 +0530

    iife

---
+++ 01-basics/14_IIFE.js
@@ -0,0 +1,38 @@
+//IMMIDIATELY INVOKED FUNCTION EXPRESSION(IIFE)
+// -> execute a function immidiately
+// -> to remove pollution due to variables in global scope we use iife
+
+/*
+(function tro(){ //nammed iife
+    console.log(`db connected`);
+})();// this (;) is very important,because the iif dont't know where to stop the context so we use ; to terminate the function run 
+
+const cunt=function(){
+    console.log("judo");
+}
+cunt()// this will give output
+
+function uto(){
+    console.log("hello")
+}
+uto();// this will give output
+
+(function aurcode (){ //if the semicolone will not there after previous function call then the next iff will not run 
+    console.log(`yo yo`)
+})();
+
+((name)=>{ //unnamed iife
+    console.log(`my database name is ${name}`);
+})("nigga")
+*/
+
+
+
+
+
+
+
+
+
+
+
