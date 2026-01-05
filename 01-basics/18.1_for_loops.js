6dc80a97783a0ef88f243af2d8a79cb4de26952e
ABHI <physicsevil007@gmail.com>
2026-01-06 03:09:36 +0530

    forloop

---
+++ 01-basics/18.1_for_loops.js
@@ -0,0 +1,41 @@
+/*for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element%2==0) console.log(element);
+}*/
+
+/*for(let i=0; i<3; i++){
+    for(let j=0; j<3 ; j++)
+        console.log(` (${i},${j})`);
+    console.log("\n");   
+}*/
+
+/*const arr=["flash","batman","superman"]
+console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}*/
+
+/*for(let i=0; i<10 ; i++){
+    const ele=i
+    if(ele==5){
+        console.log("we found fiveeeeee")
+        break   //break the if condition and whole loop
+    }
+    console.log(ele);
+}*/
+
+/*for (let index = 1; index <=20; index++) {
+    if(index==5){
+        console.log(`value of i is ${index}`);
+        continue // just jump the founded value
+    }
+    console.log(index);
+}*/
+
+
+
+
+
+
