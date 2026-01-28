a5625116400b005b621629283a24bef7b9071674
ABHI <physicsevil007@gmail.com>
2026-01-28 09:24:42 +0530

    yes

---
+++ projects/pro1.js
@@ -0,0 +1,26 @@
+const colorbox=document.querySelectorAll('.colorbox')
+const body=document.querySelector("body")
+const q2=document.querySelector('#q2')
+colorbox.forEach(function(colorbox){
+    console.log(colorbox);
+    colorbox.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==='pink') {
+            body.style.backgroundColor=e.target.id;
+            q2.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id === 'plum'){
+            q2.style.backgroundColor=e.target.id;
+            body.style.backgroundColor=e.target.id;
+        }
+        else if(e.target.id === 'bisque'){
+            q2.style.backgroundColor=e.target.id;
+            body.style.backgroundColor=e.target.id;
+        } 
+        else if(e.target.id === 'paleturquoise'){
+            q2.style.backgroundColor=e.target.id;
+            body.style.backgroundColor=e.target.id;
+        }  
+    })
+})
