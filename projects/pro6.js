86d9502358a4166e7239f3561e325ea1ffe463d6
ABHI <physicsevil007@gmail.com>
2026-02-19 00:49:19 +0530

    ok

---
+++ projects/pro6.js
@@ -0,0 +1,22 @@
+const randomcolor= function(){
+    const hex='0123456789ABCDEFG'
+    let color="#"
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+/*
+const startchangingcolor=function (){
+    setInterval(changebgcolor,1000)
+    function changebgcolor(){
+        document.body.style.backgroundColor=randomcolor();
+    }  
+};
+const stopchangingcolor=function (){}
+
+document.querySelector('#start').addEventListener('click',startchangingcolor)
+
+document.querySelector('#stop').addEventListener('click',stopchangingcolor)
+*/
