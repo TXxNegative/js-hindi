be41b28265b97a9c9766f5e7a06fa01b68f4e668
ABHI <physicsevil007@gmail.com>
2026-03-21 01:08:47 +0530

    ok

--- class_and_oop/prototype.js
+++ class_and_oop/prototype.js
@@ -1,7 +1,23 @@
 let myname="hitesh        "
 
-console.log(myname.length);
+//console.log(myname.trim().length); noobs way to print the nmber of char in a character
 
+let myhero=["thor","spiderman"]
+let heropower={
+    thor : "hammer",
+    spiderman:"sling",
+
+    getspiderpower:function(){
+        console.log(`spidey power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh=function(){
+    console.log('hitesh is present in all subjects');
+}
+
+myhero.hitesh()
+//heropower.hitesh()
 
 
 
