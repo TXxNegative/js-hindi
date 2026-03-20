56f6625f7d72db18e10885d84b017ad3c10efc51
ABHI <physicsevil007@gmail.com>
2026-03-21 03:09:24 +0530

    prototype

--- class_and_oop/prototype.js
+++ class_and_oop/prototype.js
@@ -12,12 +12,48 @@ let heropower={
     }
 }
 
-Object.prototype.hitesh=function(){
+Object.prototype.hitesh=function(){ //if we use prototype with the object then every object,array,function,string have hitesh as a property in them.
+// but if we use prototype with function or string or array individually then the data type of the protype only can use the proparty, not other data type.
     console.log('hitesh is present in all subjects');
 }
 
 myhero.hitesh()
 //heropower.hitesh()
+
+const user={
+    name:"chai",
+    email:"mp@gmail.com"
+}
+const teacher={
+    makevideo:true
+}
+
+const teachersupport={
+    isavailable:false
+}
+
+const tasupport={
+    makeassignment:"js assignment",
+    fulltime:true,
+    __proto__:teachersupport// it is a old method to access proparties of other objcts
+}
+
+teacher.__proto__=user//old method of accessing other object proparties.
+
+//moddern method ofinheritance(__proto__)
+Object.setPrototypeOf(teachersupport,teacher)
+
+let anotherusername="chaiaurcode     "
+String.prototype.truelength=function(){
+    console.log(`${this}`);
+    console.log(`${this.length}`);
+    console.log(`true length is${this.trim().length}`);
+}
+
+anotherusername.truelength()// here we use function as a property of string so we can use with any string.
+
+"hitesh".truelength()
+"icetea".truelength()
 
 
 
