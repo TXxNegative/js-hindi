aacae855b46c8986c77721af3f3ad490da07cfc6
ABHI <physicsevil007@gmail.com>
2026-07-22 12:40:44 +0530

    new

---
+++ advance js/timer_interval.js
@@ -0,0 +1,12 @@
+// let x = setInterval(function(){},(time in milli second)) //-> it run multiple times after activate. it run the program which we write inside function.
+let si=setInterval(function(){
+    
+},5000)
+
+//let y = setTimeout(function(){},(time in milli second)) //->it will only run once
+
+//clearTimeout(y) //->remove the timeout of any operation, and stop the operation permanently
+
+//clearInterval(x) //-> remove the interval of any operation, nd stop the operation permanently
+
+
