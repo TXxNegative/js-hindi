a0362aaf5954eade29763aea1823d607fd184e2a
ABHI <physicsevil007@gmail.com>
2026-09-24 17:51:50 +0530

    Update pro1.js
    done

--- projects/pro1.js
+++ projects/pro1.js
@@ -4,8 +4,12 @@ const q2=document.querySelector('#q2')
 colorbox.forEach(function(colorbox){
     console.log(colorbox);
     colorbox.addEventListener('click',function(e){
+        console.log(`value of e is${e}`);
         console.log(e);
+        
+        console.log(`value of e.target is${e.target}`);
         console.log(e.target);
+        
         if(e.target.id==='pink') {
             body.style.backgroundColor=e.target.id;
             q2.style.backgroundColor=e.target.id;
